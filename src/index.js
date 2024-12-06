037d738d7af4d3fb3990385d5cb1e5b74bb14ab1
Pakkapong2 <pmiswnnil@gmail.com>
2024-12-06 11:56:22 +0700

    update now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
